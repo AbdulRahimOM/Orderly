4e13caf0cf413d1bb617f5d38dd046ade3ca02bb
AbdulRahimOM <abdulrahimovingal@gmail.com>
2025-01-04 09:48:16 +0530

    api initialised

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,8 +2,43 @@ package main
 
 import (
 	"fmt"
+	"log"
+	route "orderly/internal/api/routes"
+	"orderly/internal/infrastructure/config"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
+func healthCheck(c *fiber.Ctx) error {
+	return c.Status(200).JSON(fiber.Map{
+		"msg": "ok",
+	})
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+
+	app := fiber.New(fiber.Config{
+		AppName:       "Ordely",
+		StrictRouting: true,
+	})
+	app.Use(logger.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     config.Configs.Env.CORSAllowedOrigins,
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		AllowMethods:     "GET, POST, PUT, DELETE, PATCH",
+		AllowCredentials: true, // Enable credentials for specific origins
+	}))
+
+	// health check
+	app.Get("/health", healthCheck)
+
+	// mount other routes
+	route.MountRoutes(app)
+
+	err := app.Listen(fmt.Sprintf(":%s", config.Configs.Env.Port))
+	if err != nil {
+		log.Fatal("Couldn't start the server. Error: " + err.Error())
+	}
 }
---
+++ internal/api/routes/init.go
@@ -0,0 +1,13 @@
+package routes
+
+import "github.com/gofiber/fiber/v2"
+
+func MountRoutes(app *fiber.App) {
+	
+
+	// for _, val := range app.Stack() {
+	// 	for _, route := range val{
+	// 		fmt.Println(route)
+	// 	}
+	// }
+}
